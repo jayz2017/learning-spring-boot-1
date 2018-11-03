fbd4e7dd7678cdab3738f87cfe317271da829c26
Justin <1012478704@qq.com>
2018-11-03 21:05:42 +0800

    init

---
+++ src/main/java/com/example/learningspringboot1/LearningSpringBoot1Application.java
@@ -0,0 +1,12 @@
+package com.example.learningspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBoot1Application.class, args);
+	}
+}
